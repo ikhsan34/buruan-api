34ab31e076f9be5aa5249c7a13f51daec1674db2
Ikhsan <ikhsanfirdaus74@gmail.com>
2022-12-21 15:24:51 +0700

    timezone

--- app/Config/App.php
+++ app/Config/App.php
@@ -108,7 +108,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $appTimezone = 'Asia/Calcutta';
+    public $appTimezone = 'Asia/Jakarta';
 
     /**
      * --------------------------------------------------------------------------
